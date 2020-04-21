784e7bcc4f17b940c141987ea3a64a745dd116d0
Cameron Maske <cameronmaske@gmail.com>
2020-04-21 10:55:52 +0200

    Include nodert geolocation package

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
+const { Geolocator } = require('@nodert-win10-rs3/windows.devices.geolocation');
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
 	// eslint-disable-line global-require
